8cab5122b6a5b660cc35130b0b90f2979f01a6d2
kalana kithmina <e19198@eng.pdn.ac.lk>
2022-12-25 12:54:07 +0530

    try

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
-fi
-cd /EvaMaria
+cd ~/
+mkdir bot
+cd bot
+echo "Cloning main Repository"
+git clone https://github.com/kalanakt/baesuzy.git
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
