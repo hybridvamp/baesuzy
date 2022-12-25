61d2ef4cb449f5329fa0e20b3b7242f61ee76e9a
kalana kithmina <e19198@eng.pdn.ac.lk>
2022-12-25 13:04:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-cd ~/
-echo "Cloning main Repository"
-git clone https://github.com/kalanakt/baesuzy.git
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/kalanakt/baesuzy.git /Baesuzy
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Baesuzy
+fi
+cd /Baesuzy
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
