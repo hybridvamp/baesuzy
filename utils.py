183223f192a32361a4d1357711a327c2a11f515c
kalanakt <e19198@eng.pdn.ac.lk>
2022-12-25 12:49:48 +0530

    fix error in low python versions

--- utils.py
+++ utils.py
@@ -347,7 +347,8 @@ def split_list(l, n):
 def get_file_id(msg: Message):
     if msg.media:
         for message_type in ("photo", "animation", "audio", "document", "video", "video_note", "voice", "sticker"):
-            if obj := getattr(msg, message_type):
+            obj = getattr(msg, message_type)
+            if obj:
                 setattr(obj, "message_type", message_type)
                 return obj
 
